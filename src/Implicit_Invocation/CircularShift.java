826a336a0c57cd59f535cfd1e844d6e9147c871e
ShumShum93 <a0121823@u.nus.edu>
2016-08-24 21:25:20 +0800

    Clean up CircularShift activation function

--- src/Implicit_Invocation/CircularShift.java
+++ src/Implicit_Invocation/CircularShift.java
@@ -9,48 +9,49 @@ import java.util.StringTokenizer;
 //shifts each line n number of times based of number of key words and
 //send each shifter line to alphabetizer
 public class CircularShift {
-	public String line;
+	private Queue<String> ignore;
 	private boolean[] keywordPos;
 	
-	public CircularShift(String line){
-		this.line = line;
+	public CircularShift(Queue<String> ignore){
+		this.ignore = ignore;
 	}
 	
 	//returns an ArrayList of all shifted keyword headings of that input line
-	public ArrayList<String> activate(Queue<String> ignore){
-		numKeywords(ignore);
-		ArrayList<String> shiftedLines = new ArrayList<String>();
+	public ArrayList<String> activate(String oneLine, ArrayList<String> ready){
+		numKeywords(oneLine);
+		Alphabetize a = new Alphabetize();
 		//loop through all words of that line and store the line in the new arraylist
 		for(int i=0;i<keywordPos.length;i++){
 			if(keywordPos[i]){
-				shiftedLines.add(this.line);
+				ready.add(oneLine);
+				ready = a.activate(ready);
 			}
-			shift();
+			oneLine = shift(oneLine);
 		}
-		return shiftedLines;
+		return ready;
 	}
 	
 	//shifts the word position by one in the line
-	private String shift(){
-		StringTokenizer tokenizer = new StringTokenizer(this.line);
+	private String shift(String line){
+		StringTokenizer tokenizer = new StringTokenizer(line);
 		String first = tokenizer.nextToken();
 		String newLine = "";
 		while(tokenizer.hasMoreTokens()){
 			newLine = newLine + tokenizer.nextToken() + " "; 
 		}
-		this.line = newLine + first;
-		return this.line;
+		line = newLine + first;
+		return line;
 	}
 	
 	//fills up keywordPos with a list of positions of the keywords
-	private void numKeywords(Queue<String> ignoreWords){
+	private void numKeywords(String line){
 		int count = 0;
 		StringTokenizer tokenizer = new StringTokenizer(line);
 		this.keywordPos = new boolean[tokenizer.countTokens()];
 		Arrays.fill(keywordPos, Boolean.FALSE);
 		while(tokenizer.hasMoreTokens()){
 			String nextToken = tokenizer.nextToken();
-			if(isKeyword(ignoreWords, nextToken)){
+			if(isKeyword(this.ignore, nextToken)){
 				keywordPos[count] = true;
 			}
 			count++;
