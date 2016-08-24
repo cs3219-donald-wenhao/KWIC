ff99c5eb4ea75b3404b51e43275ef0082c9c07f3
ShumShum93 <a0121823@u.nus.edu>
2016-08-24 15:07:30 +0800

    Implicit Invocation Master update

--- src/Implicit_Invocation/Master.java
+++ src/Implicit_Invocation/Master.java
@@ -9,11 +9,37 @@ public class Master{
 	String ignoreFile;
 	String outputFile;
 	
+	public Master(String inputFileName, String ignoreFileName, String outputFileName) {
+		// TODO Auto-generated constructor stub
+		this.inputFile = inputFileName;
+		this.ignoreFile = ignoreFileName;
+		this.outputFile = outputFileName;
+	}
+
 	public void launch(){
 		InputReader input = new InputReader();
 		
+		
 		try{
-			input.read("a.pdf");
+			//reads input and ignore files and store them in arraylist
+			input.readIgnore(ignoreFile);
+			input.readText(inputFile);
+			
+			//processing the input into circular list line by line to be processes in inputReader
+/*			while(!input.inputList.isEmpty()){
+				CircularShift shift = new CircularShift(input.inputList.get(0));
+				//shift.activate();
+				
+				input.inputList.remove(0);
+			}
+*/			
+			//***** Test Functions
+			String test = "She is a very fat mom";
+			CircularShift testShift = new CircularShift(test);
+			testShift.numKeywords(input.ignore);
+			System.out.println("Done");
+			//******
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
