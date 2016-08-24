6b0464cef03856595daf2feaf548b36177c2e491
ShumShum93 <a0121823@u.nus.edu>
2016-08-24 15:07:47 +0800

    Implicit Invocation Input Reader update

--- src/Implicit_Invocation/InputReader.java
+++ src/Implicit_Invocation/InputReader.java
@@ -4,32 +4,62 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class InputReader {
 	
-	public String[] ignore;
-	public ArrayList<String> input;
-
+	//list of words to ignore
+	public Queue<String> ignore	= new LinkedList<String>();
 	
-	public ArrayList<String> read(String fileName) throws IOException {
+	//list of input lines
+	public ArrayList<String> inputList = new ArrayList<String>();
+	
+	//list of lines ready for output
+	public ArrayList<String> sortedReady = new ArrayList<String>();
+	
+	//read ignore file and stores each word in array ignore
+	public void readIgnore(String fileName) throws IOException {
 		FileReader fin = new FileReader(fileName);
 		BufferedReader bin = new BufferedReader(fin);
 		
-		ArrayList<String> input = new ArrayList<String>();
+		String line;
+		
+		while( (line = bin.readLine()) != null) {
+			if (line.length() == 0) {
+				// reach a new line => end of input
+				break;
+			} else {
+				this.ignore.offer(line);
+			}
+		}
+		
+		fin.close();
+		bin.close();
+	}
+	
+	//reads input file and stores each word in ArrayList input
+	public void readText(String fileName) throws IOException {
+		FileReader fin = new FileReader(fileName);
+		BufferedReader bin = new BufferedReader(fin);
+		
+		inputList = new ArrayList<String>();
 		String line;
 		while( (line = bin.readLine()) != null) {
 			if (line.length() == 0) {
 				// reach a new line => end of input
 				break;
 			} else {
-				input.add(line);
+				this.inputList.add(line);
 			}
 		}
 		
 		fin.close();
 		bin.close();
-		
-		return input;		
 	}
 	
+	//activate to fill up ArrayList sortedReady
+	public ArrayList<String> activate(){
+		
+	}
 }
