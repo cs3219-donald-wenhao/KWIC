4bedb07ca025913c0a5f410db63c444d9a0144f5
wenhaohao <lwen.hao93@gmail.com>
2016-08-29 07:57:17 +0800

    Added Filter and Pipe structure

---
+++ src/pipes_and_filters/Filter.java
@@ -0,0 +1,63 @@
+package pipes_and_filters;
+
+import java.io.EOFException;
+
+public abstract class Filter {
+	
+	// List of ignored words
+	public static final String ignoreList[] = { "the", "and", "a", "to", "of", "in",
+			"i", "is", "that", "it", "on", "you", "this", "for", "but", "with",
+			"are", "have", "be", "at", "or", "as", "was", "so", "if", "out",
+			"not", "after"};
+	
+	protected Pipe in, out;
+	
+	/**
+	 * Method to set the input pipe
+	 * 
+	 * @param in
+	 */
+	public void setInPipe(Pipe in) {
+		this.in = in;
+	}
+	
+	/**
+	 * Method to set the output pipe
+	 * 
+	 * @param out
+	 */
+	public void setOutPipe(Pipe out) {
+		this.out = out;
+	}
+	
+	/**
+	 * Method to write into output pipe
+	 * 
+	 * @param s
+	 */
+	public void write(String s) {
+		if (s == null) {
+			out.close();
+			return;
+		}
+		out.write(s);
+	}
+	
+	/**
+	 * Method to read from input pipe
+	 * 
+	 * @return
+	 * @throws EOFException
+	 */
+	public String read() throws EOFException {
+		return in.read();
+	}
+	
+	/**
+	 * Method to be implemented by the respective filters
+	 */
+	public void run() {
+		
+	}
+	
+}
--- src/pipes_and_filters/Pipe.java
+++ src/pipes_and_filters/Pipe.java
@@ -1,97 +1,56 @@
 package pipes_and_filters;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.io.EOFException;
+import java.util.LinkedList;
 
 public class Pipe {
-	
-	// Initialization of name strings
-	String inputFileName;
-	String ignoreFileName;
-	String outputFileName;
+
+	public LinkedList<String> buffer;
+	private boolean closed;
 	
 	/**
 	 * Constructor
+	 */
+	public Pipe() {
+		buffer = new LinkedList<String>();
+		closed = false;
+	}
+	
+	/**
+	 * Method to write into pipe
 	 * 
-	 * @param inputFileName
-	 * @param ignoreFileName
-	 * @param outputFileName
+	 * @param data
 	 */
-	public Pipe(String inputFileName, String ignoreFileName, String outputFileName) {
-		this.inputFileName = inputFileName;
-		this.ignoreFileName = ignoreFileName;
-		this.outputFileName = outputFileName;
+	public void write(String data) {
+		if (closed) {
+			return;
+		}
+		buffer.add(data);
 	}
 	
-	
 	/**
-	 * Method to carry out the KWIC. 
+	 * Method to read from pipe
+	 * 
+	 * @return
+	 * @throws EOFException
 	 */
-	public void launch() {
-		
-		// Initiation of the various Filters used
-		InputReader reader = new InputReader();
-		CircularShift shifter = new CircularShift();
-		Alphabetizer alphabetizer = new Alphabetizer();
-		OutputWriter writer = new OutputWriter();
-		
-		ArrayList<String> input = new ArrayList<String>();
-		ArrayList<String> ignore = new ArrayList<String>();
-		
-		try {
-			// Reading input
-			input = reader.read(inputFileName);
-			ignore = reader.read(ignoreFileName);
-			
-			// Alphabetize ignored words
-			for (int i = 0; i < input.size(); i++) {
-				input.set(i, alphabetizer.alphabetize(ignore, input.get(i)));
+	public String read() throws EOFException {
+		while(true) {
+			if (buffer.isEmpty()) {
+				if (closed) {
+					throw new EOFException();
+				}
+			} else {
+				return buffer.pop();
 			}
-			
-			// Shifting the words
-			ArrayList<String> shiftedLines =  shifter.shift(ignore, input);
-			
-			// Sorting the lines
-			Collections.sort(shiftedLines, new StringCompare());
-			
-			// Writing to output
-			writer.write(outputFileName, shiftedLines);
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-
-		
 	}
 	
 	/**
-	 * Private comparator class to sort the lines, regardless of the case
+	 * Method to close pipe
 	 */
-	private class StringCompare  implements Comparator<String> {
-		
-		public int compare(String obj1, String obj2) {
-			
-			obj1 = obj1.toUpperCase();
-			obj2 = obj2.toUpperCase();
-			
-		    if (obj1 == obj2) {
-		        return 0;
-		    }
-		    
-		    if (obj1 == null) {
-		        return -1;
-		    }
-		    
-		    if (obj2 == null) {
-		        return 1;
-		    }
-		    
-		    return obj1.compareTo(obj2);
-		    
-		}
+	public void close() {
+		closed = true;
 	}
+	
 }
